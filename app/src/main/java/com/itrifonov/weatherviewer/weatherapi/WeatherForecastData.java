c007201b6197c7618b984fdc06790b8b8cc046da
TrIgor74 <trigor74@gmail.com>
2015-11-19 17:36:52 +0200

    work on weather api

---
+++ app/src/main/java/com/itrifonov/weatherviewer/weatherapi/WeatherForecastData.java
@@ -0,0 +1,15 @@
+package com.itrifonov.weatherviewer.weatherapi;
+
+public class WeatherForecastData {
+    private String mCity;
+
+
+    private class HTTPClient {
+        private static String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+        private static String IMG_URL = "http://openweathermap.org/img/w/";
+        private static String IMG_EXT = ".png";
+        private static String API_KEY = "&appid=28bfbe7a35614f03ddaaf3b091f2a414";
+
+
+    }
+}
