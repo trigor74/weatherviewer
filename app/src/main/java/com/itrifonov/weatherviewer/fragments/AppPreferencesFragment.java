8abc96841b548ef0eb08172e7e7ef26aaf376606
TrIgor74 <trigor74@gmail.com>
2015-12-25 15:58:35 +0200

    Fixed preferences summary

--- app/src/main/java/com/itrifonov/weatherviewer/fragments/AppPreferencesFragment.java
+++ app/src/main/java/com/itrifonov/weatherviewer/fragments/AppPreferencesFragment.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import com.itrifonov.weatherviewer.R;
 import com.itrifonov.weatherviewer.services.ServiceHelper;
@@ -14,6 +15,16 @@ public class AppPreferencesFragment extends PreferenceFragment
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        findPreference(getString(R.string.settings_appid_key))
+                .setSummary(preferences.getString(getString(R.string.settings_appid_key), getString(R.string.settings_appid_default)));
+        findPreference(getString(R.string.settings_city_key))
+                .setSummary(preferences.getString(getString(R.string.settings_city_key), getString(R.string.settings_city_default)));
+        findPreference(getString(R.string.settings_units_key))
+                .setSummary(preferences.getString(getString(R.string.settings_units_key), getString(R.string.settings_units_default)));
+        findPreference(getString(R.string.settings_language_key))
+                .setSummary(preferences.getString(getString(R.string.settings_language_key), getString(R.string.settings_language_default)));
     }
 
     @Override
@@ -35,16 +46,15 @@ public class AppPreferencesFragment extends PreferenceFragment
         if (key.equals(getString(R.string.settings_language_key))) {
             Preference langPref = findPreference(key);
             langPref.setSummary(sharedPreferences.getString(key, ""));
-        } else {
-            if (key.equals(getString(R.string.settings_show_notifications_key))) {
-                Boolean showNotifications = sharedPreferences.getBoolean(
-                        getString(R.string.settings_show_notifications_key),
-                        getResources().getBoolean(R.bool.settings_show_notifications_default));
-                if (showNotifications) {
-                    ServiceHelper.getInstance(getActivity().getApplicationContext()).startNotificationService();
-                } else {
-                    ServiceHelper.getInstance(getActivity().getApplicationContext()).stopNotificationService();
-                }
+        }
+        if (key.equals(getString(R.string.settings_show_notifications_key))) {
+            Boolean showNotifications = sharedPreferences.getBoolean(
+                    getString(R.string.settings_show_notifications_key),
+                    getResources().getBoolean(R.bool.settings_show_notifications_default));
+            if (showNotifications) {
+                ServiceHelper.getInstance(getActivity().getApplicationContext()).startNotificationService();
+            } else {
+                ServiceHelper.getInstance(getActivity().getApplicationContext()).stopNotificationService();
             }
         }
     }
