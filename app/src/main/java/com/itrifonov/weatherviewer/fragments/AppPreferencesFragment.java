21c228c839097859f34c75feafa18567b57d9f03
Igor Trifonov <trigor74@gmail.com>
2015-12-27 23:37:35 +0200

    Find city by location service in progress

--- app/src/main/java/com/itrifonov/weatherviewer/fragments/AppPreferencesFragment.java
+++ app/src/main/java/com/itrifonov/weatherviewer/fragments/AppPreferencesFragment.java
@@ -2,6 +2,7 @@ package com.itrifonov.weatherviewer.fragments;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
@@ -36,6 +37,25 @@ public class AppPreferencesFragment extends PreferenceFragment
         findPreference(getString(R.string.settings_notification_interval_key))
                 .setSummary(preferences.getString(getString(R.string.settings_notification_interval_key),
                         getString(R.string.settings_notification_interval_default)));
+
+        findPreference(getString(R.string.settings_search_city_key))
+                .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        // TODO: 27.12.15 add search city logic
+                        /*
+                        http://api.openweathermap.org/data/2.5/find?lat=40.9479&lon=40&cnt=1&appid=28bfbe7a35614f03ddaaf3b091f2a414
+
+                        {"message":"accurate","cod":"200","count":1,"list":[{"id":751971,"name":"Arakli","coord":{"lon":40.058418,"lat":40.938541},"main":{"temp":284.15,"pressure":1033,"humidity":62,"temp_min":284.15,"temp_max":284.15},"dt":1451245800,"wind":{"speed":1.5,"deg":170,"var_beg":130,"var_end":210},"sys":{"country":""},"clouds":{"all":0},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}]}]}
+
+                        {"message":"bad request","cod":"500"}
+
+                        {"message":"exception: Legacy point is out of bounds for spherical query","cod":"500"}
+                        */
+                        findPreference(getString(R.string.settings_search_city_key)).setSummary("result");
+                        return true;
+                    }
+                });
     }
 
     @Override
